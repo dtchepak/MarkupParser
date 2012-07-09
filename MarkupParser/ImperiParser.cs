404d52406c73bd6ee7380259176fd258de9d94b9
Michael Dubs <mdubs@cochlear.com>
2012-07-09 11:08:32 +1000

    Working

--- MarkupParser/ImperiParser.cs
+++ MarkupParser/ImperiParser.cs
@@ -34,8 +34,8 @@ namespace MarkupParser
             var node = Parse("hi *world*");
 
             node.Children().Count().ShouldBe(2);
-            var text = node.Children().First();
             var bold = node.Children().Skip(1).First();
+            var text = node.Children().First();
             text.ToString().ShouldBe("(TEXT: hi )");
             bold.Children().Count().ShouldBe(1);
             bold.Children().First().ToString().ShouldBe("(TEXT: world)");
@@ -59,19 +59,61 @@ namespace MarkupParser
 
         public class TreeNode
         {
+            public enum Result
+            {
+                Continue,
+                Failed,
+                Closed
+            }
+
             private readonly List<TreeNode> _children = new List<TreeNode>();
+            private TreeNode _currentChild;
             public TreeNode Parent { get; set; }
             public IEnumerable<TreeNode> Children() { return _children; }
-            public void Add(TreeNode node) {
+            public void Add(TreeNode node)
+            {
                 _children.Add(node);
                 node.Parent = this;
             }
             public virtual bool IsEnd(char c) { return false; }
+
+            public virtual Result Parse(char c)
+            {
+                if (_currentChild == null)
+                {
+                    if (c == '*')
+                    {
+                        _currentChild = new BoldTreeNode();
+                        Add(_currentChild);
+                        return Result.Continue;
+                    }
+
+                    _currentChild = new TextTreeNode();
+                    Add(_currentChild);
+                }
+
+                var result = _currentChild.Parse(c);
+
+                if (result == Result.Closed)
+                {
+                    _currentChild = null;
+                    return Result.Continue;
+                }
+                if (result == Result.Failed)
+                {
+                    _currentChild = null;
+                    Parse(c);
+                }
+                return result;
+            }
         }
 
         public class BoldTreeNode : TreeNode
         {
-            public override bool IsEnd(char c) { return c == '*'; }
+            public override Result Parse(char c)
+            {
+                return c == '*' ? Result.Closed : base.Parse(c);                
+            }
         }
 
         public class TextTreeNode : TreeNode
@@ -88,44 +130,25 @@ namespace MarkupParser
             {
                 return Parent.IsEnd(c);
             }
+
+            public override Result Parse(char c)
+            {
+                if (c == '*')
+                {
+                    return Result.Failed;
+                }
+                Value += c;
+                return Result.Continue;
+            }
         }
 
         public TreeNode Parse(string s)
         {
             var root = new TreeNode();
-            var current = root;
+
             foreach (var c in s)
             {
-                if (current.IsEnd(c))
-                {
-                    current = current.Parent;
-                }
-                else if (IsStart(c))
-                {
-                    if (current.GetType() == typeof(TextTreeNode))
-                    {
-                        current = current.Parent;
-                    }
-                    //create node of that type
-                    var boldTreeNode = new BoldTreeNode();
-                    current.Add(boldTreeNode);
-                    current = boldTreeNode;
-                }
-                else
-                {
-                    if (current.GetType() != typeof(TextTreeNode))
-                    {
-                        var textNode = new TextTreeNode();
-                        textNode.Add(c);
-                        current.Add(textNode);
-                        current = textNode;
-                    }
-                    else
-                    {
-                        var textNode = (TextTreeNode) current;
-                        textNode.Add(c);
-                    }
-                }
+                root.Parse(c);
             }
 
             return root;
